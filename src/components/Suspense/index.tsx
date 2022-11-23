b437b467930fec9dbb26b35516d899913a7dc588
Carlo <crgmzlps@gmail.com>
2022-11-23 17:05:55 -0300

    mocked version

--- src/components/Suspense/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Suspense, PropsWithChildren } from "react";
-import Loading from 'components/Loading';
-
-const ConncreteSuspense = (props: PropsWithChildren<{}>) => {
-  return <Suspense fallback={<Loading />}>{props.children}</Suspense>;
-};
-
-export default ConncreteSuspense;
